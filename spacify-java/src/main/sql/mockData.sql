355ff814b77a55f34cdd0cfe6d177b61971cf02c
Viraj <desaiviraj28@gmail.com>
2023-02-26 13:35:18 -0800

    mockData

---
+++ spacify-java/src/main/sql/mockData.sql
@@ -0,0 +1,68 @@
+-- user table entries
+INSERT INTO corespacify.user VALUES('desaivh', 'admin', 'desaivh@uci.edu', 'Viraj', 'Desai');
+INSERT INTO corespacify.user VALUES('sanchg4', 'admin', 'sanchg4@uci.edu', 'Sanchi', 'Gupta');
+INSERT INTO corespacify.user VALUES('sdharank', 'admin', 'sdharank@uci.edu', 'Samruddhi', 'Dharankar');
+INSERT INTO corespacify.user VALUES('hthallam', 'student', 'hthallam@uci.edu', 'Himanshu');
+INSERT INTO corespacify.user VALUES('msrahma2', 'student', 'msrahma2@uci.edu', 'Saqib', 'Rahman');
+INSERT INTO corespacify.user VALUES('acolaco', 'faculty', 'acolaco@uci.edu', 'Ashwin', 'Colaco');
+INSERT INTO corespacify.user VALUES('sshekoka', 'student', 'sshekoka@uci.edu', 'Sarang', 'Shekokar');
+INSERT INTO corespacify.user VALUES('diazvi', 'student', 'diazvi@uci.edu', 'Victor');
+INSERT INTO corespacify.user VALUES('kshatris', 'student', 'kshatris@uci.edu', 'Sarthak');
+INSERT INTO corespacify.user VALUES('chinmank', 'student', 'chinmank@uci.edu', 'Chinmay', 'Kulkarni');
+INSERT INTO corespacify.user VALUES('dmohanku', 'student', 'dmohanku@uci.edu', 'DJ', 'Kumar');
+INSERT INTO corespacify.user VALUES('sharad', 'faculty', 'sharad@ics.uci.edu', 'Sharad', 'Mehrotra');
+
+-- room table entries
+
+INSERT INTO corespacify.room VALUES(1, 'something', 'study', 12);
+INSERT INTO corespacify.room VALUES(2, '', 'study', 55);
+INSERT INTO corespacify.room VALUES(3, 'something else', 'study', 5);
+INSERT INTO corespacify.room VALUES(4, null, 'office', 9);
+INSERT INTO corespacify.room VALUES(5, null, 'office', 80);
+INSERT INTO corespacify.room VALUES(6, null, 'common space', 70);
+INSERT INTO corespacify.room VALUES(7, '', 'study', 20);
+INSERT INTO corespacify.room VALUES(8, 'no more than 10 people', 'conference', 24);
+INSERT INTO corespacify.room VALUES(9, 'blah blah blah', 'study', 23);
+INSERT INTO corespacify.room VALUES(10, null, 'study', 17);
+
+-- owner table entries
+
+INSERT INTO corespacify.owner VALUES(1, 'desaivh');
+INSERT INTO corespacify.owner VALUES(1, 'sdharank');
+INSERT INTO corespacify.owner VALUES(5, 'acolaco');
+INSERT INTO corespacify.owner VALUES(8, 'sanchg4');
+INSERT INTO corespacify.owner VALUES(4, 'sharad');
+
+-- subscriber table entries
+
+INSERT INTO corespacify.subscriber VALUES(1, 'sanchg4');
+INSERT INTO corespacify.subscriber VALUES(1, 'sshekoka');
+INSERT INTO corespacify.subscriber VALUES(1, 'kshatris');
+INSERT INTO corespacify.subscriber VALUES(8, 'chinmank');
+INSERT INTO corespacify.subscriber VALUES(8, 'dmohanku');
+INSERT INTO corespacify.subscriber VALUES(2, 'hthallam');
+INSERT INTO corespacify.subscriber VALUES(2, 'acolaco');
+INSERT INTO corespacify.subscriber VALUES(8, 'desaivh');
+
+-- reservation table entries
+
+INSERT INTO corespacify.reservation VALUES(1, 8, '2023-02-22 00:00:00', '2023-02-22 19:13:29.36106', 7, 'desaivh');
+INSERT INTO corespacify.reservation VALUES(2, 4, '2023-02-22 11:00:00', '2023-02-22 14:00:00', 2, 'sanchg4');
+INSERT INTO corespacify.reservation VALUES(3, 5, '2023-02-20 12:00:00', '2023-02-20 13:00:00', 9, 'hthallam');
+INSERT INTO corespacify.reservation VALUES(5, 2, '2023-01-28 00:00:00', '2028-01-28 01:30:00', 9, 'sdharank');
+INSERT INTO corespacify.reservation VALUES(6, 15, '2023-02-03 15:30:00', '2023-02-20 18:00:00', 1, 'msrahma2');
+INSERT INTO corespacify.reservation VALUES(7, null, '2023-02-22 00:00:00', '2023-02-20 18:00:00', 1, 'dmohanku');
+INSERT INTO corespacify.reservation VALUES(8, null, '2023-02-18 10:00:00', '2023-02-20 14:00:00', 8, 'desaivh');
+
+-- incentive table entries
+
+INSERT INTO corespacify.incentive VALUES(1, 10, '2023-02-23 02:00:00', 'sanchg4');
+INSERT INTO corespacify.incentive VALUES(2, 10, '2023-02-23 02:00:02', 'desaivh');
+INSERT INTO corespacify.incentive VALUES(3, 15, '2023-02-23 02:00:20', 'dmohanku');
+INSERT INTO corespacify.incentive VALUES(4, 5, '2023-02-21 02:00:00', 'hthallam');
+INSERT INTO corespacify.incentive VALUES(5, 10, '2023-01-29 02:00:00', 'sdharank');
+INSERT INTO corespacify.incentive VALUES(6, 15, '2023-02-04 02:00:00', 'msrahma2');
+INSERT INTO corespacify.incentive VALUES(7, 15, '2023-02-19 02:00:00', 'desaivh');
+
+-- monitoring table entries
+-- TODO: finalize the table's structure based on the final API contract.
