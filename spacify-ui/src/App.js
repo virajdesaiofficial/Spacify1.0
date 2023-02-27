6e26d1f5b4502ab2f2dfa8ec44a474394c8f794a
Viraj <desaiviraj28@gmail.com>
2023-02-26 16:16:02 -0800

    added navigation bar

--- spacify-ui/src/App.js
+++ spacify-ui/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/Nav/Navigation";
+import Home from "./components/Home/Home";
+import Create from "./components/CreateRoom/Create";
+import Team from "./components/team/Team";
+import React from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        element: <Navigation />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/create",
+                element: <Create />
+            },
+            {
+                path: "/team",
+                element: <Team />
+            }
+        ]
+    }
+    ]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Go team TechTippers! Spacify build starts
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+          <RouterProvider router={router}/>
+      </div>
   );
 }
 
