861589788315da9699183c392e6803518af0cf6d
Viraj <desaiviraj28@gmail.com>
2023-02-26 17:12:08 -0800

    refactoring

---
+++ spacify-ui/src/components/reservation/Reservation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './reservation.css';
+
+function Reservation(props) {
+    return (
+        <div>reservation page</div>
+    );
+}
+
+export default Reservation;
