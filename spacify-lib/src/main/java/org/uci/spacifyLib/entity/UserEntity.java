45fa2be5761180ce52227655c82bfbece607273d
Viraj <desaiviraj28@gmail.com>
2023-02-27 00:00:50 -0800

    fixing access_level enum values

--- spacify-lib/src/main/java/org/uci/spacifyLib/entity/UserEntity.java
+++ spacify-lib/src/main/java/org/uci/spacifyLib/entity/UserEntity.java
@@ -2,6 +2,7 @@ package org.uci.spacifyLib.entity;
 
 import com.sun.istack.NotNull;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -32,6 +33,7 @@ public class UserEntity {
     private String lastName;
 
     @Column(name = "access_level", columnDefinition = "VARCHAR(20)")
+    @Enumerated(EnumType.STRING)
     private AccessLevel accessLevel;
 
     public UserEntity(String userId, String email, String firstName, String lastName, AccessLevel accessLevel) {
