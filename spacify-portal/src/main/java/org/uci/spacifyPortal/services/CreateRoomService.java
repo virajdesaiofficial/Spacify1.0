a74d75289cebcafcd1a1ecb7394e86e265f8f2a9
HimanshuThallam <123609037+HimanshuThallam@users.noreply.github.com>
2023-03-07 23:27:47 -0800

    Revert "Update CreateRoomService.java"
    This reverts commit c95fa464160a5a5116ee2095c5f894c1fa70a366.

--- spacify-portal/src/main/java/org/uci/spacifyPortal/services/CreateRoomService.java
+++ spacify-portal/src/main/java/org/uci/spacifyPortal/services/CreateRoomService.java
@@ -19,7 +19,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 @Service
-public class RoomService {
+public class CreateRoomService {
 
     @Autowired
     private OwnerRepository ownerRepository;
