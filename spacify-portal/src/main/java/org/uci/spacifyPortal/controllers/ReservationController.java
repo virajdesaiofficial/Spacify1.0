42612d65593b3bdafb3fff842cdd4231eea915fc
HimanshuThallam <123609037+HimanshuThallam@users.noreply.github.com>
2023-04-21 11:40:50 -0700

    dummy commit to verify git

--- spacify-portal/src/main/java/org/uci/spacifyPortal/controllers/ReservationController.java
+++ spacify-portal/src/main/java/org/uci/spacifyPortal/controllers/ReservationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.uci.spacifyLib.repsitory.IncentiveRepository;
 import org.uci.spacifyPortal.services.ReservationService;
 import org.uci.spacifyLib.entity.ReservationEntity;
 
@@ -14,6 +15,9 @@ import java.util.List;
 public class ReservationController {
     @Autowired
     private ReservationService reservationService;
+
+    @Autowired
+    private IncentiveRepository incentiveRepository;
 
     @GetMapping("/all")
     public List<ReservationEntity> getAllReservations() {
